2f945b49e2319ea220fb470f48b4de579fba827d
Ted Jenkler <tedjenkler@gmail.com>
2024-08-22 00:31:15 +0200

    feat: Make portfolio page responsive for tablet and desktop

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -10,7 +10,7 @@ function PortfolioContainer() {
     { name: "Project Del Sol", img: "portfolio4.png", imgmd: "portfolio4md.png", imgxl: "portfolio4xl.png", year: "2016", month: "January" },
     { name: "Le Prototype", img: "portfolio5.png", imgmd: "portfolio5md.png", imgxl: "portfolio5xl.png", year: "2015", month: "October" },
     { name: "228B Tower", img: "portfolio6.png", imgmd: "portfolio6md.png", imgxl: "portfolio6xl.png", year: "2015", month: "April" },
-    { name: "Grand Edelweiss Hotel", img: "portfolio7.png", imgmd: "portfolio7md.png", imgxl: "portfolio7xl.png", year: "2013", month: "December" },
+    { name: "Grand Edelweiss", img: "portfolio7.png", imgmd: "portfolio7md.png", imgxl: "portfolio7xl.png", year: "2013", month: "December" },
     { name: "Netcry Tower", img: "portfolio8.png", imgmd: "portfolio8md.png", imgxl: "portfolio8xl.png", year: "2012", month: "August" },
     { name: "Hypers", img: "portfolio9.png", imgmd: "portfolio9md.png", imgxl: "portfolio9xl.png", year: "2012", month: "January" },
     { name: "SXIV Tower", img: "portfolio10.png", imgmd: "portfolio10md.png", imgxl: "portfolio10xl.png", year: "2011", month: "March" },
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import PortfolioContainer from '../components/PortfolioContainer'
+import tabletdesign2 from '../assets/images/tabletdesign2.png'
 
 function Portfolio() {
   return (
     <main className='portfoliopage'>
+        <img className='tabletdesign' src={tabletdesign2} alt='tablet design' />
         <PortfolioContainer />
     </main>
   )
