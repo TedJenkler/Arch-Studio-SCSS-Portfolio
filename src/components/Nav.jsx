340f88c12e9f8417f5a2c71a920976e28a3f5dc2
Ted Jenkler <tedjenkler@gmail.com>
2024-08-23 23:20:13 +0200

    feat: Add and style mobile navigation; animations to be added later

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 import burger from "../assets/images/burger.svg";
 import logo from "../assets/images/logo.svg";
 import { Link } from 'react-router-dom';
 
 function Nav() {
+
+  const [menu, setMenu] = useState(false);
+
   return (
-    <nav>
+    <>
+      <nav>
         <Link to="/"><img className='logo' src={logo} alt='logo' /></Link>
         <ul>
-          <Link to="/portfolio">Portfolio</Link>
-          <Link to="/about">About Us</Link>
-          <Link to="/contact">Contact</Link>
+          <li><Link to="/portfolio">Portfolio</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
-        <img className='burger' src={burger} alt='burger' />
-    </nav>
-  )
+        <img onClick={() => setMenu(!menu)} className='burger' src={burger} alt='burger' />
+      </nav>
+      {menu && (
+        <div className='mobilemenu'>
+          <ul>
+            <li><Link to="/portfolio" onClick={() => setMenu(false)}>Portfolio</Link></li>
+            <li><Link to="/about" onClick={() => setMenu(false)}>About Us</Link></li>
+            <li><Link to="/contact" onClick={() => setMenu(false)}>Contact</Link></li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Nav
+export default Nav;
