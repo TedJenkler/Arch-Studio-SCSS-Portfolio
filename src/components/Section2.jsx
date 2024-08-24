8d3fbc52c3d13a69d6be93cf46e8324dce8b79e0
Ted Jenkler <tedjenkler@gmail.com>
2024-08-25 00:44:05 +0200

    fix: Resolve styling issues at 320px breakpoint

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -8,7 +8,9 @@ function Section2() {
         <div className='design-'></div>
         <h2>Our Heritage</h2>
         <p>Founded in 2007, we started as a trio of architects. Our complimentary skills and relentless attention to detail turned Arch into one of the most sought after boutique firms in the country.</p>
+        <br></br>
         <p>Specializing in Urban Design allowed us to focus on creating exceptional structures that live in harmony with their surroundings. We consider every detail from every surrounding element to inform our designs.</p>
+        <br></br>
         <p>Our small team of world-class professionals provides input on every project.</p>
       </div>
       <img src={section2img} alt='Our team of architects working on projects' className='section2-img' />
