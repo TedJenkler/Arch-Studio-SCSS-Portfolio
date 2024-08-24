6794f733d170d20b1c2303f03a2630f3ba5d2ec3
Ted Jenkler <tedjenkler@gmail.com>
2024-08-24 03:42:02 +0200

    feat: Add hover effect to About section to display social media links

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import linkedin from '../assets/images/linkedin.svg';
+import twitter from '../assets/images/twitter.svg';
 
 function Person({ img, h2, p }) {
   return (
     <div className='person'>
       <img src={img} alt={h2}/>
+      <div className='social'>
+        <img src={linkedin} alt='linkdin' />
+        <img src={twitter} alt='twitter' />
+      </div>
       <div>
         <h2>{h2}</h2>
         <p>{p}</p>
